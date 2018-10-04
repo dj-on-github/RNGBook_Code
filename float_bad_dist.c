ebfafbaf5e5f382df9f35c995d2a046a5469a21b
David Johnston <dj@deadhat.com>
2018-10-04 08:19:53 -0700

    Added bins param to float_bad_dist.c

--- float_bad_dist.c
+++ float_bad_dist.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <math.h>
 
-uint64_t histogram[100];
-
+#define MAXBINS 1000000
+#define ITERATIONS 1000000
 
 uint64_t get_random_bits(FILE *f) {
     uint64_t x;
@@ -21,16 +21,16 @@ uint64_t choose_exponent(FILE *f) {
     return e;
 }
 
-void clear_histogram() {
+void clear_histogram(uint64_t *histogram) {
     int i;
-    for (i=0;i<100;i++) histogram[i]=0;
+    for (i=0;i<MAXBINS;i++) histogram[i]=0;
 }
 
-void add_to_histogram(double f) {
+void add_to_histogram(uint64_t *histogram, int bins, double f) {
     double x;
     int i;
-    x = f * 100.0;
-    for (i=0;i<100;i++) {
+    x = f * (double)bins;
+    for (i=0;i<bins;i++) {
         if (x <= ((double)(i+1))) {
             histogram[i] += 1;
             break;
@@ -39,7 +39,7 @@ void add_to_histogram(double f) {
     return;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     uint64_t start;
     uint64_t mantissa;
     uint64_t exponent;
@@ -47,22 +47,39 @@ int main() {
     uint64_t x;
     double *fp;
     double f;
+    double max;
+    double min;
+    int bins;
     int i;
+
+    uint64_t histogram[MAXBINS];
     
+    if (argc > 1) bins = atoi(argv[1]);
+    else bins = 10;
+    if (bins > MAXBINS) {
+        printf("Error, to many bins. The maximum is %d\n",MAXBINS);
+        exit(-1);
+    }
+
     FILE *dur;
     dur = fopen("/dev/urandom", "rb");
 
-    clear_histogram();
-    for (i=0;i<1000000;i++) {
+    clear_histogram(histogram);
+    for (i=0;i<ITERATIONS;i++) {
         mantissa = get_random_bits(dur) & 0x0fffffffffffff;
         exponent = choose_exponent(dur);
         sign = 0;
         x = (sign << 63) | (exponent << 52) | mantissa;
         fp = (double *)&x;
         f = *fp;
-        add_to_histogram(f);
+        add_to_histogram(histogram,bins,f);
     }
-    for (i=0;i<100;i++) printf("%llu\n",histogram[i]);
+
+    for (i=0;i<10;i++) {
+        min = (i*1.0)/bins;
+        max = ((i+1)*1.0)/bins;
+        printf("%1.8f to %1.8f :  %llu\n",min,max,histogram[i]);
+    }
     fclose(dur);
     return 1;
 }
